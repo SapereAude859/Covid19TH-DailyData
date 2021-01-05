108eecd7d33e736c04c3f0f4e44367ccd985e240
PluZ <geruby85@gmail.com>
2021-01-05 19:02:02 +0700

    Add Away-Covid 14 Days

--- script/week_analyze.py
+++ script/week_analyze.py
@@ -37,6 +37,6 @@ class WeekAnalyze:
     def away(self):
         self.df["date_start"] = pd.to_datetime(self.df["date_start"], format="%Y-%m-%d")
         df_week = self.df[
-            self.df.date > datetime.datetime.now() - pd.to_timedelta("14day")
+            self.df.date_start > datetime.datetime.now() - pd.to_timedelta("14day")
         ]
         return df_week
