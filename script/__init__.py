18f8e8dce84a47f31c422e4ca43600eae2e4db29
PluZ <geruby85@gmail.com>
2021-04-27 22:46:10 +0700

    Remove data.go.th csv

--- script/__init__.py
+++ script/__init__.py
@@ -9,10 +9,10 @@ print(
 )
 
 endpoints = ["cases", "timeline", "area"]
-url = (
-    "https://data.go.th/dataset/8a956917-436d-4afd-a2d4-59e4dd8e906e/resource/d838f9b0-f8ad-4223-86c7-c380f8589da1"
-    "/download/pm-21-04-64v1.csv "
-)
+# url = (
+#     "https://data.go.th/dataset/8a956917-436d-4afd-a2d4-59e4dd8e906e/resource/d838f9b0-f8ad-4223-86c7-c380f8589da1"
+#     "/download/pm-21-04-64v1.csv "
+# )
 sheets = ["data", "watchout", "risk", "patient"]
 away_url = "https://docs.google.com/spreadsheets/u/0/d/11Gx-Wc2bXb2pAcwKT4jcuLLZ0BYoCrjixo54UxX3KTw/pubhtml"
 
@@ -38,13 +38,13 @@ for endpoint in endpoints:
         api_data = Import("https://covid19.th-stat.com/api/open/" + endpoint).daily()
         Export(api_data, endpoint, "dataset")
 
-    data_gov = Import(url).gov()
-    Export(data_gov, "Data-Gov", "dataset")
-try:
-    data_gov_week = Week(data_gov).gov()
-    Export(data_gov_week, "data-gov-week", "Recent 14Days Alert Data🔥")
-except TypeError:
-    pass
+# data_gov = Import(url).gov()
+# Export(data_gov, "Data-Gov", "dataset")
+# try:
+#     data_gov_week = Week(data_gov).gov()
+#     Export(data_gov_week, "data-gov-week", "Recent 14Days Alert Data🔥")
+# except TypeError:
+#     pass
 
 away_covid = Import(away_url).away()
 Export(away_covid, "away-covid", "dataset")
